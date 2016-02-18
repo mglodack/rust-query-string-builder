1813595a1113f883b47cba61d2e5456e4a01aa4d
Mathew Glodack <glodack@gmail.com>
2016-02-17 21:22:31 -0800

    Use single fold
    * Create intialize query method to instantiate the
    first query of the list of kvps

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,32 @@
 pub fn build_query(params: Vec<(&str, &str)>) -> String {
     if params.is_empty() { return String::new() }
 
-    let first = _assign_single_kvp(params.first());
-    let last = _assign_multiple_kvp(params);
-
-    format!("?{0}{1}", first, last)
-}
-
-fn _assign_single_kvp(kvp: Option<&(&str, &str)>) -> String {
-    match kvp {
-        Some(kvp) => format!("{0}={1}", kvp.0, kvp.1),
-        None => String::new()
-    }
-}
-
-fn _assign_multiple_kvp(params: Vec<(&str, &str)>) -> String {
     params.iter()
         .skip(1)
         .fold(
-            String::new(),
-            | mut query, kvp | {
+             _initialize_query(params.first()),
+             | mut query, kvp | {
                 query.push_str(&format!("&{0}={1}", kvp.0, kvp.1));
                 query
-            })
+             })
+}
+
+fn _initialize_query(kvp: Option<&(&str, &str)>) -> String {
+    match kvp {
+        Some(kvp) => format!("?{0}={1}", kvp.0, kvp.1),
+        None => String::new()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn build_query_with_single_kvp() {
+        let params = vec![("key1", "value1")];
+        assert_eq!("?key1=value1", build_query(params))
+    }
 
     #[test]
     fn it_works() {
